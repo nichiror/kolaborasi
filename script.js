748a971c48710a3b1ea6c0b82e1c6b83b30a6525
Nicole 9B 25 <nicole.suciadi@gmail.com>
2025-04-30 11:22:10 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-elementImg = document.getElementById("IdImage");
-listImage = ["bg.jpg", "background.jpg", "b3.jpg", "bg4.jpg"];
-i = 0;
-function gantiGbr(){
-elementImg.src = listImage[i];
-i = (i + 1) % listImage.length;
-}
-setInterval(gantiGbr,5000)
