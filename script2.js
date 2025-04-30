a6adf38b052c5a6f23ce18343d6bf6d8457d79d8
Nicole 9B 25 <nicole.suciadi@gmail.com>
2025-04-30 11:22:30 +0700

    Delete script2.js

--- script2.js
+++
@@ -1,8 +0,0 @@
-elementImg = document.getElementById("Idimage");
-listImage = ["store.jpg", "store1.jpg", "store2.jpg"];
-i = 0;
-function gantiGbr(){
-elementImg.src = listImage[i];
-i = (i + 1) % listImage.length;
-}
-setInterval(gantiGbr,5000)
